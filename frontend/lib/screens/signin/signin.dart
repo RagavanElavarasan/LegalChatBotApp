d1c18f69b79418d64db7833e16d6712c9e2f4e7b
Pavithra Raja <pavithraraja374@gmail.com>
2024-10-09 09:36:37 +0530

    Sign in page

--- frontend/lib/screens/signin/signin.dart
+++ frontend/lib/screens/signin/signin.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/chat_interface/chat_interface.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:http/http.dart' as http; // Add this import
-import 'dart:convert'; // For JSON encoding
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
@@ -13,11 +12,11 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
-  // Variables to store email and password
   String email = '';
   String password = '';
+  bool rememberMe = false;
+  bool obscurePassword = true; // New boolean to track password visibility
 
-  // Function to handle sign-in
   Future<void> _signIn() async {
     if (email.isEmpty || password.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -26,17 +25,14 @@ class _SigninPageState extends State<SigninPage> {
       return;
     }
 
-    // API URL (replace with your backend URL)
-    const String apiUrl = 'http://10.0.2.2:5001/login'; // Adjust the URL
+    const String apiUrl = 'http://10.0.2.2:5001/login';
 
-    // Prepare the data for the request
     final Map<String, dynamic> data = {
       'email': email,
       'password': password,
     };
 
     try {
-      // Send POST request to Flask backend
       final response = await http.post(
         Uri.parse(apiUrl),
         headers: {'Content-Type': 'application/json'},
@@ -46,7 +42,6 @@ class _SigninPageState extends State<SigninPage> {
       if (response.statusCode == 200) {
         final responseBody = jsonDecode(response.body);
         Get.to(() => ChatInterfacePage());
-        // Handle successful login (navigate to another screen, store user info, etc.)
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Login failed: ${response.body}')),
@@ -66,142 +61,172 @@ class _SigninPageState extends State<SigninPage> {
         automaticallyImplyLeading: false,
         title: const Text("Copsify AI"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 100),
-              child: Text(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200, // Set your desired height
+                width: 200, // Set your desired width
+                child: Image.asset('assets/images/sign_in.png'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
                 "Sign in",
                 style: TextStyle(
-                  fontSize: 32,
+                  fontSize: 30,
                   fontFamily: "Joan",
                 ),
               ),
-            ),
-            const SizedBox(height: 50),
-            SizedBox(
-              height: 45,
-              width: 300,
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value; // Update email state
-                },
-                decoration: InputDecoration(
-                  labelText: 'Enter Your Email Address',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              height: 45,
-              width: 300,
-              child: TextField(
-                obscureText: true, // Hide password text
-                onChanged: (value) {
-                  password = value; // Update password state
-                },
-                decoration: InputDecoration(
-                  labelText: 'Enter Your Password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Align(
-              alignment: Alignment.centerLeft, // Aligns the button to the left
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 50), // Adjust the padding as necessary
-                child: TextButton(
-                  onPressed: () {
-                    // Handle password recovery
+              const SizedBox(height: 40),
+              SizedBox(
+                height: 45,
+                width: 330,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
                   },
-                  child: const Text(
-                    "Forget Password?",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 14,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Email Address',
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    labelText: 'Email',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _signIn();
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF083087),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 75, vertical: 13),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              child: const Text(
-                "Sign in",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: const [
-                Text("              "),
-                Expanded(child: Divider(thickness: 3, color: Colors.black26)),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Text("or"),
-                ),
-                Expanded(child: Divider(thickness: 3, color: Colors.black26)),
-                Text("               "),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Create account"),
-                TextButton(
-                  onPressed: () {
-                    // Handle sign-up navigation
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 45,
+                width: 330,
+                child: TextField(
+                  obscureText: obscurePassword, // Obscure text based on state
+                  onChanged: (value) {
+                    password = value;
                   },
-                  child: const Text(
-                    " Sign up",
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
-                      color: Colors.blue,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    hintText: 'Enter Your Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        // Change icon based on obscurePassword state
+                        obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                        size: 20,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          obscurePassword =
+                              !obscurePassword; // Toggle visibility
+                        });
+                      },
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 70,
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30.0), // Adjust padding as needed
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          onChanged: (newValue) {
+                            setState(() {
+                              rememberMe = newValue!;
+                            });
+                          },
+                        ),
+                        const Text('Remember me'),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Handle Forgot Password action
+                      },
+                      child: const Text('Forgot Password?'),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildSocialMediaIcon('assets/images/google.png', 30, 30),
-                const SizedBox(width: 20),
-                _buildSocialMediaIcon(
-                    'assets/images/microsoft.png', 50, 50), // Adjusted width
-                const SizedBox(width: 20),
-                _buildSocialMediaIcon('assets/images/facebook.png', 30, 30),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 47,
+                width: 330,
+                child: ElevatedButton(
+                  onPressed: _signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF083087),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Sign in",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                children: const [
+                  Text("              "),
+                  Expanded(child: Divider(thickness: 3, color: Colors.black26)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("or"),
+                  ),
+                  Expanded(child: Divider(thickness: 3, color: Colors.black26)),
+                  Text("               "),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildSocialMediaIcon('assets/images/google.png', 30, 30),
+                  const SizedBox(width: 20),
+                  _buildSocialMediaIcon('assets/images/microsoft.png', 50, 50),
+                  const SizedBox(width: 20),
+                  _buildSocialMediaIcon('assets/images/facebook.png', 30, 30),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Create account"),
+                  TextButton(
+                    onPressed: () {
+                      // Handle sign-up navigation
+                    },
+                    child: const Text(
+                      " Sign up",
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 70),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -215,7 +240,7 @@ class _SigninPageState extends State<SigninPage> {
       child: Image.asset(
         assetPath,
         height: height,
-        width: width, // Set custom width
+        width: width,
       ),
     );
   }
